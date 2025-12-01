9cd4c7aba9211a88829ea63fcb248ed760cecef0
Welton Felix <wplf@cin.ufpe.br>
2025-12-01 15:13:29 -0300

    Refactor session management and enhance appointment handling with patient details

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from datetime import date
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
+CONFIRMED_STATUS_ID = 3
 
 
 class Session:
+    username: str = os.getenv("PLATFORM_USERNAME")
+
     def __init__(self):
-        self.username = os.getenv("PLATFORM_USERNAME")
-        self.token = None
+        self.token: str = None
 
     def login(self):
         response = requests.post(
@@ -21,21 +23,82 @@ class Session:
         self.token = response.json().get("access_token")
 
 
+class Patient:
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        cellphone: str,
+        is_whatsapp: bool,
+        email: str,
+        gender: str | None,
+    ):
+        self.patient_id = id
+        self.name = name
+        self.cellphone = cellphone
+        self.is_whatsapp = is_whatsapp
+        self.email = email
+        self.gender = gender
+
+    @staticmethod
+    def from_json(json_data):
+        return Patient(
+            id=json_data["id"],
+            name=json_data["name"],
+            cellphone=json_data["cellphone"],
+            is_whatsapp=json_data["flagWhatsapp"] == 1,
+            email=json_data["email"],
+            gender=json_data["gender"],
+        )
+
+
+class Appointment:
+    def __init__(
+        self,
+        id: int,
+        date: str,
+        hour: str,
+        status: str,
+        local: str | None,
+        patient: Patient,
+    ):
+        self.appointment_id = id
+        self.date = date
+        self.hour = hour
+        self.status = status
+        self.local = local
+        self.patient = patient
+
+    @staticmethod
+    def from_json(json_data):
+        return Appointment(
+            id=json_data["id"],
+            date=json_data["date"],
+            hour=json_data["hour"],
+            status=json_data["status"]["nameGerund"],
+            local=json_data.get("local", {}).get("name", None),
+            patient=Patient.from_json(json_data["patient"]),
+        )
+
+
 session = Session()
 session.login()
 
-# Give todays date in YYYY-MM-DD format
+# Give todays date in YYYY-MM-d format
 today = date.today()
 todayDate = f"{today.year}-{today.month}-{today.day}"
 
 data = requests.get(
     f"{BASE_URL}/schedule/{todayDate}",
     headers={"Authorization": f"Bearer {session.token}"},
+    params={"status": CONFIRMED_STATUS_ID},
 ).json()
 
-appointments = data.get("data", [])
-#print(data)
+appointments = [Appointment.from_json(app) for app in data.get("data", [])]
+
+# TODO: Connect to Twilio and send messages to patients
 for appointment in appointments:
-    print(
-        f"Patient: {appointment['patient']['name']}, Time: {appointment['hour']}, Status: {appointment['status']['nameGerund']}"
-    )
+    if appointment.patient.cellphone:
+        print(
+            f"Patient: {appointment.patient.name}, Time: {appointment.hour}, Status: {appointment.status}, phone: {appointment.patient.cellphone}"
+        )
